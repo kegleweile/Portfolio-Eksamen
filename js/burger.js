f8dcc1d8f11eb3062c9e2d711859ce9b8d6199f0
Your Name <cawe0001@stud.kea.dk>
2024-06-05 11:03:24 +0200

    burger done ish

--- js/burger.js
+++ js/burger.js
@@ -1,18 +1,16 @@
+// const = forkortelse, i stedet for at skrive hele linjen.
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link"); 
 
-hamburger.addEventListener("click", mobileMenu);
-
+// hamburger class åbner og lukker krydset.
+// navmenu class åbner og lukker menuen.
 function mobileMenu() {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 }
 
-const navLink = document.querySelectorAll(".nav-link");
-
-navLink.forEach((n) => n.addEventListener("click", closeMenu));
-
-function closeMenu() {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach((elementer) => elementer.addEventListener("click", mobileMenu));
+// tilføjer eventlistener til alle elementer i listen navLink
+//gå i konsol, skriv console.log(navLink) og den viser hele listen, som man kalder på.
